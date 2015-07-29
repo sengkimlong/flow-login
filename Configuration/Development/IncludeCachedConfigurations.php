74189998a1afcbccbbe855d34487223fc0e4b099
sengkimlong <sengkimlong@ubuntu.ubuntu-domain>
2015-07-29 09:30:15 +0700

    save to database

--- Configuration/Development/IncludeCachedConfigurations.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (FLOW_PATH_ROOT !== '/var/www/html/flow-login/' || !file_exists('/var/www/html/flow-login/Data/Temporary/Development/Configuration/DevelopmentConfigurations.php')) {
-	unlink(__FILE__);
-	return array();
-}
-return require '/var/www/html/flow-login/Data/Temporary/Development/Configuration/DevelopmentConfigurations.php';
